c6b0329911f477bbe9ad4efb9cf15fe2c84fed81
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-05-30 12:54:44 +0500

    commit

---
+++ ReverseString.py
@@ -0,0 +1,20 @@
+# Function to reverse a string 
+def reverse(string):
+    string = "".join(reversed(string))
+    return string
+
+
+s = "Shahzain Ahmed"
+a = "86"
+
+print("The original string  is: ", end="")
+print(s)
+
+print("The reversed string is: ", end="")
+print(reverse(s))
+
+print ("The reversed number is: ", end="")
+print(reverse(a))
+
+
+
