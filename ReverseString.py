e14d6015da21e5f48b76538ff0e7abb8d2224e6a
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-05-30 13:38:26 +0500

    s

--- ReverseString.py
+++ ReverseString.py
@@ -10,15 +10,6 @@ name = input("Enter your desired string:\n")
 # Taking a number as an input from the user.
 number = input("Enter your desired number:\n")
 
-# Using print statement to display the original string.
-print("The original string  is: ", end="")
-print(s)
-
-print("The reversed string is: ", end="")
-print(reverse(s))
-
-print ("The reversed number is: ", end="")
-print(reverse(a))
-
-
-
+# Using print statement to display the original & reversed string.
+print("The original string is: ", name)
+print("The reversed string is: ", reverse(name))
